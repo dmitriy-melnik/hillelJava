15541e0daedea5dc60f2e309031d63e05a907aa6
Dmitriy Melnik <buddy.manlik@gmail.com>
2020-02-05 15:59:43 +0200

    Task 14

--- src/Task14/Dictionary.java
+++ src/Task14/Dictionary.java
@@ -1,18 +1,68 @@
 package Task14;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Dictionary {
-    private String word;
-    private String russianTranslate;
 
-    public void setWord(String word) {
-        this.word = word;
+    private ArrayList<Pair> words = new ArrayList<Pair>();
+
+    public void addWordPair(String english, String russian) {
+        words.add(new Pair(english, russian));
     }
 
-    public void setRussianTranslate(String russianTranslate) {
-        this.russianTranslate = russianTranslate;
+    public void deleteWordPair(String english, String russian) {
+        Iterator<Pair> wordsIterator = words.iterator();
+        while (wordsIterator.hasNext()) {
+            Pair p = wordsIterator.next();
+            if (p.getWord().equals(english) && p.getRussianTranslate().equals(russian)) {
+                wordsIterator.remove();
+            }
+        }
     }
 
-    public String getRussianTranslate() {
-        return russianTranslate;
+    public String getRussianTranslate(String english) {
+        for (Pair p : words) {
+            if (p.getWord().equals(english)) {
+                return p.getRussianTranslate();
+            }
+        }
+        return "";
+    }
+
+    @Override
+    public String toString() {
+        return words + "";
+    }
+
+    public static class Pair {
+        private String word;
+        private String russianTranslate;
+
+        public Pair(String word, String russianTranslate) {
+            this.word = word;
+            this.russianTranslate = russianTranslate;
+        }
+
+        public String getWord() {
+            return word;
+        }
+
+        public void setWord(String word) {
+            this.word = word;
+        }
+
+        public String getRussianTranslate() {
+            return russianTranslate;
+        }
+
+        public void setRussianTranslate(String russianTranslate) {
+            this.russianTranslate = russianTranslate;
+        }
+
+        @Override
+        public String toString() {
+            return word + " - " + russianTranslate;
+        }
     }
 }
--- src/Task14/Main.java
+++ src/Task14/Main.java
@@ -1,4 +1,14 @@
 package Task14;
 
 public class Main {
+    public static void main(String[] args) {
+        Dictionary firstDictionary = new Dictionary();
+        firstDictionary.addWordPair("hello", "привет");
+        firstDictionary.addWordPair("cat", "кот");
+        firstDictionary.addWordPair("book", "книга");
+        System.out.println(firstDictionary);
+        /*firstDictionary.deleteWordPair("hello", "привет");
+        System.out.println(firstDictionary);*/
+        System.out.println(firstDictionary.getRussianTranslate("hello"));
+    }
 }
