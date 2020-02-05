25ac241ee2dcba60c5d0e73bfe4eb8a10f8046f4
Dmitriy Melnik <buddy.manlik@gmail.com>
2020-02-05 15:07:09 +0200

    Task 14

---
+++ src/Task14/Dictionary.java
@@ -0,0 +1,18 @@
+package Task14;
+
+public class Dictionary {
+    private String word;
+    private String russianTranslate;
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public void setRussianTranslate(String russianTranslate) {
+        this.russianTranslate = russianTranslate;
+    }
+
+    public String getRussianTranslate() {
+        return russianTranslate;
+    }
+}
