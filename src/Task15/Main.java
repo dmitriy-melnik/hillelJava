5579721a696772f3f1250a5a734a9d733c391179
Dmitriy Melnik <buddy.manlik@gmail.com>
2020-02-14 22:58:55 +0200

    Task 15 (edited)

--- src/Task15/Main.java
+++ src/Task15/Main.java
@@ -12,7 +12,6 @@ public class Main {
         System.out.println("glass" + "glass".hashCode() % 10);
 */
 
-        System.out.println("Git commit");
         HashMapIml map = new HashMapIml();
         map.put("hello", "привет");
         map.put("car", "автомобиль");
